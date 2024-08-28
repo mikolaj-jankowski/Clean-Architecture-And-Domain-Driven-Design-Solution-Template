5f0739149b236b6483cd2f3ecf90c49661ed1222
mikolaj.jankowski <mikolaj.jankowski@cyfra.gov.pl>
2024-08-28 13:02:53 +0200

    removed unnecessary code

--- content/src/Clean.Architecture.And.DDD.Template.Domian/Orders/DomainEvents/OrderCreatedDomainEvent.cs
+++ content/src/Clean.Architecture.And.DDD.Template.Domian/Orders/DomainEvents/OrderCreatedDomainEvent.cs
@@ -2,18 +2,5 @@
 
 namespace Clean.Architecture.And.DDD.Template.Domian.Orders.DomainEvents
 {
-    //public record OrderCreatedDomainEvent(OrderId OrderId, CustomerId CustomerId) : IDomainEvent;
-    //public record OrderCreatedDomainEvent(Guid OrderId, Guid CustomerId) : IDomainEvent;
-
-    public class OrderCreatedDomainEvent : IDomainEvent
-    {
-        public OrderCreatedDomainEvent(Guid orderId, Guid customerId)
-        {
-            OrderId = orderId;
-            CustomerId = customerId;
-        }
-
-        public Guid OrderId { get; set; }
-        public Guid CustomerId { get; set; }
-    }
+    public record OrderCreatedDomainEvent(Guid OrderId, Guid CustomerId) : IDomainEvent;
 }
