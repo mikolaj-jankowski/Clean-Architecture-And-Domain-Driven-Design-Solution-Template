61682685994eb0e6389548bdb42dca993fa93fb0
mikolaj.jankowski <mikolaj.jankowski@cyfra.gov.pl>
2024-09-25 14:59:09 +0200

    added few unit tests

--- content/tests/Domain.UnitTests/CustomerTests.cs
+++ content/tests/Domain.UnitTests/CustomerTests.cs
@@ -17,6 +17,27 @@ namespace Domain.UnitTests
             });
         }
 
+        [Theory]
+        [InlineData("AB")]
+        [InlineData("Jonathan Alexander Montgomery-Harrington Fitzwilliam Cunningham-Smithington Kensington Wetherby-Clarkson Paddington-Livingston O'Sullivan Carrington-Rutherford")]
+        internal void Should_Throw_Invalid_FullName_Domain_Exception_For_Too_Short_Or_Too_Long_FullName(string fullName)
+        {
+            Assert.Throws<InvalidFullNameDomainException>(() =>
+            {
+                new FullName(fullName);
+            });
+        }
+
+        [Fact]
+        internal void Should_Throw_Invalid_Age_Domain_Exception_When_Age_Is_Under_18_Years_Old()
+        {
+            Assert.Throws<InvalidCustomerAgeDomainException>(() =>
+            {
+                new Age(DateTime.UtcNow.AddYears(-16));
+            });
+        }
+
+
         [Fact]
         internal void Should_Create_Customer_For_Valid_Input_Data()
         {
@@ -43,7 +64,64 @@ namespace Domain.UnitTests
             var domainEvent = domainEvents.FirstOrDefault();
             Assert.NotNull(domainEvent);
             Assert.IsType<CustomerCreatedDomainEvent>(domainEvent); 
+        }
 
+        [Fact]
+        internal void Should_Change_Customer_Email()
+        {
+            // Arrange
+            var customerId = new CustomerId(Guid.NewGuid());
+            var fullName = new FullName("Mikolaj Jankowski");
+            var age = new Age(DateTime.UtcNow.AddYears(-20));
+            var email = new Email("my-email@yahoo.com");
+            var address = new Address("Fifth Avenue", "10A", "1", "USA", "10037");
+
+            // Act
+            var customer = Clean.Architecture.And.DDD.Template.Domian.Customers.Customer.CreateCustomer(
+                customerId,
+                fullName,
+                age,
+                email,
+                address);
+
+            customer.ChangeEmail(new Email("new-email-address@yahoo.com"));
+
+            // Assert
+            var domainEvents = customer.DomainEvents;
+            Assert.NotNull(domainEvents);
+            Assert.Equal(2, domainEvents.Count);
+
+            Assert.Single(domainEvents.OfType<CustomerCreatedDomainEvent>());
+            Assert.Single(domainEvents.OfType<CustomerEmailChnagedDomainEvent>());
+        }
+
+        [Fact]
+        internal void Should_Verify_Customer_Email()
+        {
+            // Arrange
+            var customerId = new CustomerId(Guid.NewGuid());
+            var fullName = new FullName("Mikolaj Jankowski");
+            var age = new Age(DateTime.UtcNow.AddYears(-20));
+            var email = new Email("my-email@yahoo.com");
+            var address = new Address("Fifth Avenue", "10A", "1", "USA", "10037");
+
+            // Act
+            var customer = Clean.Architecture.And.DDD.Template.Domian.Customers.Customer.CreateCustomer(
+                customerId,
+                fullName,
+                age,
+                email,
+                address);
+
+            customer.VerifyEmailAddress();
+
+            // Assert
+            var domainEvents = customer.DomainEvents;
+            Assert.NotNull(domainEvents);
+            Assert.Equal(2, domainEvents.Count);
+
+            Assert.Single(domainEvents.OfType<CustomerCreatedDomainEvent>());
+            Assert.Single(domainEvents.OfType<CustomerEmailVerifiedDomainEvent>());
         }
     }
 }
---
+++ content/tests/Domain.UnitTests/OrderTests.cs
@@ -0,0 +1,39 @@
+using Clean.Architecture.And.DDD.Template.Domian.Customers;
+using Clean.Architecture.And.DDD.Template.Domian.Orders;
+using Clean.Architecture.And.DDD.Template.Domian.Orders.DomainEvents;
+using Clean.Architecture.And.DDD.Template.Domian.Orders.Exceptions;
+
+namespace Domain.UnitTests
+{
+    public class OrderTests
+    {
+        [Fact]
+        internal void Should_Create_Order_For_Valid_Input_Data()
+        {
+            // Arrange
+            var customerId = new CustomerId(Guid.NewGuid());
+            var shippingAddress = new ShippingAddress("Fifth Avenue 10A", "10037");
+
+            // Act
+            var order = Order.Create(customerId, shippingAddress);
+
+            // Assert
+            var domainEvents = order.DomainEvents;
+            Assert.Single(domainEvents.OfType<OrderCreatedDomainEvent>());
+        }
+
+        [Fact]
+        internal void Should_Throw_Maximum_Quantity_Exceeded_Domain_Exception_When_Quantity_Is_Higher_Then_5()
+        {
+            // Arrange
+            var customerId = new CustomerId(Guid.NewGuid());
+            var shippingAddress = new ShippingAddress("Fifth Avenue 10A", "10037");
+
+            // Act && Assert
+            Assert.Throws<MaximumQuantityExceededDomainException>(() =>
+            {
+                Order.Create(customerId, shippingAddress).AddOrderItem(1, "Tent", 100, "USD", 6);
+            });
+        }
+    }
+}
